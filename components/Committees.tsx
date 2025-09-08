a1afd92008c8c2a5c0d779659efd7b06bd1e0d16
wnsah814 <sjmskm@gmail.com>
2025-09-08 10:26:31 +0900

    Update committee

--- components/Committees.tsx
+++ components/Committees.tsx
@@ -5,7 +5,7 @@ const Committees = () => {
         <span className="inline-block px-3 py-1 text-sm font-medium text-blue-800 bg-blue-100 rounded-full mb-3">ICFEC 2026 Committees</span>
         <h2 className="text-3xl md:text-4xl font-bold mb-10 text-center">Organization</h2>
       </div>
-      
+
       <div className="max-w-5xl mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12">
           <div className="col-span-1">
@@ -25,7 +25,7 @@ const Committees = () => {
               </li>
             </ul>
           </div>
-          
+
           <div className="col-span-1">
             <h4 className="text-lg font-medium mb-4 border-b border-gray-200 pb-2">Program Chairs</h4>
             <ul className="space-y-3">
@@ -96,62 +96,135 @@ const Committees = () => {
             </ul>
           </div>
 
-          
-          
-          {/* <div className="col-span-2">
+
+        </div>
+
+        <div className="mt-16">
+          <div className="col-span-1 md:col-span-3">
             <h4 className="text-lg font-medium mb-4 border-b border-gray-200 pb-2">Program Committee</h4>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
-              <div>
-                <ul className="space-y-3">
-                  <li>
-                    <a href="#" className="text-blue-600 hover:underline">Member Name</a>
-                    <p className="text-sm text-gray-600">Affiliation, Country</p>
-                  </li>
-                  <li>
-                    <a href="#" className="text-blue-600 hover:underline">Member Name</a>
-                    <p className="text-sm text-gray-600">Affiliation, Country</p>
-                  </li>
-                  <li>
-                    <a href="#" className="text-blue-600 hover:underline">Member Name</a>
-                    <p className="text-sm text-gray-600">Affiliation, Country</p>
-                  </li>
-                  <li>
-                    <a href="#" className="text-blue-600 hover:underline">Member Name</a>
-                    <p className="text-sm text-gray-600">Affiliation, Country</p>
-                  </li>
-                  <li>
-                    <a href="#" className="text-blue-600 hover:underline">Member Name</a>
-                    <p className="text-sm text-gray-600">Affiliation, Country</p>
-                  </li>
-                </ul>
-              </div>
-              
-              <div>
-                <ul className="space-y-3">
-                  <li>
-                    <a href="#" className="text-blue-600 hover:underline">Member Name</a>
-                    <p className="text-sm text-gray-600">Affiliation, Country</p>
-                  </li>
-                  <li>
-                    <a href="#" className="text-blue-600 hover:underline">Member Name</a>
-                    <p className="text-sm text-gray-600">Affiliation, Country</p>
-                  </li>
-                  <li>
-                    <a href="#" className="text-blue-600 hover:underline">Member Name</a>
-                    <p className="text-sm text-gray-600">Affiliation, Country</p>
-                  </li>
-                  <li>
-                    <a href="#" className="text-blue-600 hover:underline">Member Name</a>
-                    <p className="text-sm text-gray-600">Affiliation, Country</p>
-                  </li>
-                  <li>
-                    <a href="#" className="text-blue-600 hover:underline">Member Name</a>
-                    <p className="text-sm text-gray-600">Affiliation, Country</p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div> */}
+            <ul className="space-y-3 columns-1 sm:columns-2 lg:columns-3 xl:columns-4">
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Sungyong Ahn</a>
+                <p className="text-sm text-gray-600">Pusan National University, South Korea</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Atakan Aral</a>
+                <p className="text-sm text-gray-600">University of Vienna, Austria</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Christian Becker</a>
+                <p className="text-sm text-gray-600">University of Stuttgart, Germany</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">David Bermbach</a>
+                <p className="text-sm text-gray-600">TU Berlin, Germany</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Luiz F. Bittencourt</a>
+                <p className="text-sm text-gray-600">University of Campinas, Brazil</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Eddy Caron</a>
+                <p className="text-sm text-gray-600">ENS-Lyon / INRIA / LIP, France</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Byungjin Cho</a>
+                <p className="text-sm text-gray-600">Korea University, South Korea</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Alexandre da Silva Veith</a>
+                <p className="text-sm text-gray-600">Nokia Bell Labs, Belgium</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Schahram Dustdar</a>
+                <p className="text-sm text-gray-600">Vienna University of Technology, Austria</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Janick Edinger</a>
+                <p className="text-sm text-gray-600">Universität Hamburg, Germany</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Lidia Fotia</a>
+                <p className="text-sm text-gray-600">University of Salerno, Italy</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Antonino Galletta</a>
+                <p className="text-sm text-gray-600">University of Messina, Italy</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Sukhpal Singh Gill</a>
+                <p className="text-sm text-gray-600">Queen Mary University of London, United Kingdom</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Daniel Grosu</a>
+                <p className="text-sm text-gray-600">Wayne State University, United States</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Qiang He</a>
+                <p className="text-sm text-gray-600">Huazhong University of Science and Technology, China</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Zhiyi Huang</a>
+                <p className="text-sm text-gray-600">University of Otago, New Zealand</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Bahman Javadi</a>
+                <p className="text-sm text-gray-600">Western Sydney University, Australia</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Odej Kao</a>
+                <p className="text-sm text-gray-600">TU Berlin, Germany</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Vasileios Karagiannis</a>
+                <p className="text-sm text-gray-600">Austrian Institute of Technology, Austria</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Dragi Kimovski</a>
+                <p className="text-sm text-gray-600">University of Klagenfurt, Austria</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Anna Kobusinska</a>
+                <p className="text-sm text-gray-600">Poznan University of Technology, Poland</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Chandra Krintz</a>
+                <p className="text-sm text-gray-600">UC Santa Barbara, United States</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Joonho Kwon</a>
+                <p className="text-sm text-gray-600">Pusan National University, South Korea</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Seng Loke</a>
+                <p className="text-sm text-gray-600">Deakin University, Australia</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Adrien Lèbre</a>
+                <p className="text-sm text-gray-600">Inria / Ecole des Mines, France</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Narges Mehran</a>
+                <p className="text-sm text-gray-600">Salzburg Research Ltd. / University of Salzburg, Austria</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Zahra Najafabadi Samani</a>
+                <p className="text-sm text-gray-600">University of Klagenfurt, Austria</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Guillaume Pierre</a>
+                <p className="text-sm text-gray-600">Univ Rennes, Inria, CNRS, IRISA, France</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Radu Prodan</a>
+                <p className="text-sm text-gray-600">University of Innsbruck, Austria</p>
+              </li>
+              <li className="break-inside-avoid">
+                <a href="#" className="text-blue-600 hover:underline">Lin Wang</a>
+                <p className="text-sm text-gray-600">Paderborn University, Germany</p>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
